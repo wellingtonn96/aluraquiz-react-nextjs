1fadd8a3901887da195b3ab40ef7c3145b2480d8
Wellington Santos <weltossousa@gmail.com>
2021-02-07 15:00:51 -0300

    configuring eslint prettier and editorconfig

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <h1>Hello World!</h1>
+      </Head>
+    </div>
+  )
+}
+
+export default Home
