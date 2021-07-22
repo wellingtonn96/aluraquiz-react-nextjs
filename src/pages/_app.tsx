5b0ad7629a100a68a8b40148958bed42257ecba0
Wellington Santos <weltossousa@gmail.com>
2021-07-21 22:32:29 -0300

    adding github corner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { AppProps } from 'next/app';
-
+import { AppProps } from 'next/app'
 
 import GlobalStyle from '../styles/global'
 import { ThemeProvider } from 'styled-components'
-import theme from '../styles/theme';
+import GithubCorner from 'react-github-corner'
+import theme from '../styles/theme'
+import { UTILS } from '../constants/utils'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </ ThemeProvider>
+      <GithubCorner bannerColor={theme.colors.mainBg} href={UTILS.github} />
+    </ThemeProvider>
   )
 }
 
