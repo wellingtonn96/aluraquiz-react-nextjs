0839a9bb5cf8f22b8f179568c867848e65224304
Wellington Santos <weltossousa@gmail.com>
2021-08-29 18:12:50 -0300

    adding feature to update theme

--- packages/server/src/modules/theme/theme.controller.ts
+++ packages/server/src/modules/theme/theme.controller.ts
@@ -1,21 +1,31 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateThemeQuizDTO } from './dto/CreateThemeQuizDTO';
 import { ThemeQuiz } from './entities/theme.repository';
-import { ThemeQuizService } from './theme.service';
+import { IThemeUpdate, ThemeQuizService } from './theme.service';
 
 @Controller('themeQuiz')
 export class ThemeQuizController {
-  constructor(private ThemeQuizService: ThemeQuizService) {}
+  constructor(private themeQuizService: ThemeQuizService) {}
 
   @Post()
   async createQuestion(
     @Body() CreateThemeQuizDTO: CreateThemeQuizDTO,
   ): Promise<ThemeQuiz> {
-    return await this.ThemeQuizService.createThemeQuiz(CreateThemeQuizDTO);
+    return await this.themeQuizService.createThemeQuiz(CreateThemeQuizDTO);
   }
 
   @Get()
   async getAllThemeQuiz(): Promise<ThemeQuiz[]> {
-    return await this.ThemeQuizService.getThemeQuiz();
+    return await this.themeQuizService.getThemeQuiz();
+  }
+
+  @Get('/:id')
+  async getOneThemeById(@Param('id') id: string) {
+    return await this.themeQuizService.getOneThemeById(id);
+  }
+
+  @Put('/:id')
+  async updateTheme(@Param('id') id: string, @Body() data: IThemeUpdate) {
+    return await this.themeQuizService.updateThemeById({ id, theme: data });
   }
 }
--- packages/server/src/modules/theme/theme.service.ts
+++ packages/server/src/modules/theme/theme.service.ts
@@ -4,22 +4,57 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ThemeQuiz } from './entities/theme.repository';
 import { ThemeQuizRepository } from './theme.repository';
 
+export interface IThemeUpdate {
+  primary: string;
+  mainBg: string;
+  wrong: string;
+  success: string;
+  contrastText: string;
+  secondary: string;
+}
+
 @Injectable()
 export class ThemeQuizService {
   constructor(
     @InjectRepository(ThemeQuizRepository)
-    private quisRepository: ThemeQuizRepository,
+    private themeRepository: ThemeQuizRepository,
   ) {}
 
   async createThemeQuiz(data: CreateThemeQuizDTO): Promise<ThemeQuiz> {
-    const thmeQuiz = this.quisRepository.create(data);
+    const thmeQuiz = this.themeRepository.create(data);
 
-    return await this.quisRepository.save(thmeQuiz);
+    return await this.themeRepository.save(thmeQuiz);
   }
 
   async getThemeQuiz(): Promise<ThemeQuiz[]> {
-    const thmeQuiz = await this.quisRepository.find();
+    const thmeQuiz = await this.themeRepository.find();
 
     return thmeQuiz;
+  }
+
+  async getOneThemeById(id: string): Promise<ThemeQuiz> {
+    const thmeQuiz = await this.themeRepository.findOne(id);
+
+    return thmeQuiz;
+  }
+
+  async updateThemeById({
+    id,
+    theme,
+  }: {
+    id: string;
+    theme: IThemeUpdate;
+  }): Promise<IThemeUpdate> {
+    const findTheme = await this.getOneThemeById(id);
+
+    if (!findTheme) {
+      throw new Error('theme with this id not exits!');
+    }
+
+    const assignTheme = Object.assign(findTheme, theme);
+
+    const themeUpdated = this.themeRepository.save(assignTheme);
+
+    return themeUpdated;
   }
 }
