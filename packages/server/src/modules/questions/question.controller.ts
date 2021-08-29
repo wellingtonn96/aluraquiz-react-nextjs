cd49d198b14c5e321056bfe29f63aed7efec9c35
Wellington Santos <weltossousa@gmail.com>
2021-08-29 18:11:54 -0300

    adding feature to update and delete questions

--- packages/server/src/modules/questions/question.controller.ts
+++ packages/server/src/modules/questions/question.controller.ts
@@ -1,20 +1,47 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateQuestionDto } from './dto/createQuestion.dto';
 import { Questions } from './entities/question.repository';
-import { QuestionService } from './question.service';
+import { IQuestionUpdate, QuestionService } from './question.service';
 
 @Controller('question')
 export class QuestionController {
-	constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService) {}
 
-	@Post()
-	async createQuestion(@Body() createQuestionDto: CreateQuestionDto): Promise<Questions> {
-		return await this.questionService.createQuestion(createQuestionDto)
-	}
+  @Post()
+  async createQuestion(
+    @Body() createQuestionDto: CreateQuestionDto,
+  ): Promise<Questions> {
+    return await this.questionService.createQuestion(createQuestionDto);
+  }
 
-	@Get()
-	async getAllQuestions(): Promise<Questions[]> {
-		return await this.questionService.getQuestions()
-	}
+  @Get()
+  async getAllQuestions(): Promise<Questions[]> {
+    return await this.questionService.getQuestions();
+  }
+
+  @Get('/:id')
+  async getOneQuiz(@Param('id') id: string): Promise<Questions> {
+    return await this.questionService.getQuestionById(id);
+  }
+
+  @Delete('/:id')
+  async removeQuestion(@Param('id') id: string): Promise<Questions> {
+    return await this.questionService.removeQuestion(id);
+  }
+
+  @Put('/:id')
+  async updateQuestion(@Param('id') id: string, @Body() data: IQuestionUpdate) {
+    return await this.questionService.updateQuestionById({
+      id,
+      question: data,
+    });
+  }
 }
-
--- packages/server/src/modules/questions/question.service.ts
+++ packages/server/src/modules/questions/question.service.ts
@@ -1,35 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/createQuestion.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Questions } from './entities/question.repository';
-import { QuestionsRepository } from './question.repository'
-import { Any } from 'typeorm';
+import { QuestionsRepository } from './question.repository';
+
+export interface IQuestionUpdate {
+  title: string;
+  image_url: string;
+  description: string;
+  answer: string;
+  quizId: string;
+  alternatives: string[];
+}
 
 @Injectable()
 export class QuestionService {
-	constructor(
-		@InjectRepository(QuestionsRepository)
-		private quisRepository: QuestionsRepository,
-	) { }
+  constructor(
+    @InjectRepository(QuestionsRepository)
+    private questionRepository: QuestionsRepository,
+  ) {}
 
-	async createQuestion(data: CreateQuestionDto): Promise<Questions> {
-		const quiz = this.quisRepository.create(data)
+  async createQuestion(data: CreateQuestionDto): Promise<Questions> {
+    const quiz = this.questionRepository.create(data);
 
-		return await this.quisRepository.save(quiz)
-	}
+    return await this.questionRepository.save(quiz);
+  }
 
-	async getQuestions(): Promise<Questions[]> {
+  async getQuestions(): Promise<Questions[]> {
+    const quiz = await this.questionRepository.find({
+      select: [
+        'description',
+        'id',
+        'image_url',
+        'title',
+        'created_at',
+        'updated_at',
+      ],
+      relations: ['quiz'],
+    });
 
-		const quiz = await this.quisRepository.find({
-			select: ['description', 'id', 'image_url', 'title', 'created_at', 'updated_at'],
-			relations: ['quiz'],
-		});
+    return quiz;
+  }
 
-		return quiz
-		// return await this.quisRepository.query(`
-		// SELECT * FROM "questions" INNER JOIN "quiz" 
-		// 		ON "quiz"."id" = "questions"."quizId" ORDER BY "quiz"."created_at"
-		// `)
+  async getQuestionById(id: string): Promise<Questions> {
+    const findQuestion = await this.questionRepository.findOne(id);
 
-	}
+    if (!findQuestion) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Question with this id not exits!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const question = await this.questionRepository.findOne({
+      select: [
+        'description',
+        'id',
+        'image_url',
+        'title',
+        'created_at',
+        'updated_at',
+      ],
+      relations: ['quiz'],
+      where: {
+        id: id,
+      },
+    });
+
+    return question;
+  }
+
+  async updateQuestionById({
+    id,
+    question,
+  }: {
+    id: string;
+    question: IQuestionUpdate;
+  }): Promise<IQuestionUpdate> {
+    const findQuestion = await this.questionRepository.findOne(id);
+
+    if (!findQuestion) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Question with this id not exits!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const assignQuestion = Object.assign(findQuestion, question);
+
+    const questionUpdated = await this.questionRepository.save(assignQuestion);
+
+    return questionUpdated;
+  }
+
+  async removeQuestion(id: string): Promise<Questions> {
+    const findQuestion = await this.questionRepository.findOne(id);
+
+    if (!findQuestion) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Question with this id not exits!',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const question = await this.questionRepository.remove(findQuestion);
+
+    return question;
+  }
 }
