6ddddffe12b342b8409588e3faacea4e8e14f3d8
Wellington Santos <weltossousa@gmail.com>
2021-08-29 18:15:14 -0300

    change rule eslint no unused vars

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
