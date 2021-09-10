f4e73c39cc7e7ddc4b0b6c74f10bc7981e1f691a
Wellington Santos <weltossousa@gmail.com>
2021-09-10 00:03:07 -0300

    adding site landing page

---
+++ packages/web/src/pages/home.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+import CardQuiz from '../components/CardQuiz'
+import { ButtonStyled } from '../components/Button/styled'
+import api from '../services/api'
+import { FiEdit, FiTrash } from 'react-icons/fi'
+import { UTILS } from '../constants/utils'
+import ReactLoading from 'react-loading'
+import theme from '../styles/theme'
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 480px) {
+    justify-content: center;
+  }
+
+  div.loading {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    justify-self: center;
+  }
+`
+
+const ButtonNewQuizContainer = styled.div`
+  padding: 40px 0;
+  display: flex;
+  flex-direction: row-reverse;
+`
+
+interface IPropsHome {
+  quizes: any[] | null
+}
+
+const HomePage: React.FC<IPropsHome> = ({ quizes }) => {
+  const router = useRouter()
+
+  function handleSubmit(id: string) {
+    return router.push(`quiz/?id=${id}`)
+  }
+
+  const toggleMenuItems = [
+    {
+      icon: <FiTrash size={20} />,
+      text: 'Deletar',
+      onclick: async (id: string) => {
+        try {
+          await api.delete(`quiz/${id}`)
+
+          router.push(`/`)
+        } catch (error) {
+          alert(JSON.stringify({ err: error.message }))
+        }
+      },
+    },
+    {
+      icon: <FiEdit size={20} />,
+      text: 'Editar',
+      onclick: async (id: string) => {
+        try {
+          router.push(`/update/${id}`)
+        } catch (error) {
+          alert(JSON.stringify({ err: error.message }))
+        }
+      },
+    },
+  ]
+
+  return (
+    <Layout padding={true}>
+      <ButtonNewQuizContainer>
+        <ButtonStyled onClick={() => router.push('/create-quiz')} width="160px">
+          Novo quiz
+        </ButtonStyled>
+      </ButtonNewQuizContainer>
+      <HomeContainer>
+        {quizes ? (
+          quizes.map(item => (
+            <CardQuiz
+              header={item.title}
+              itemId={item.id}
+              toggleItems={toggleMenuItems}
+              background={item.img_bg_url}
+              width="350px"
+            >
+              <p>{item.title}</p>
+              <span>{item.description}</span>
+
+              <ButtonStyled onClick={() => handleSubmit(item.id)} type="button">
+                Responda o quiz
+              </ButtonStyled>
+            </CardQuiz>
+          ))
+        ) : (
+          <div className="loading">
+            <ReactLoading
+              color={theme.colors.secondary}
+              type="spinningBubbles"
+            />
+          </div>
+        )}
+      </HomeContainer>
+    </Layout>
+  )
+}
+
+export default HomePage
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(`${UTILS.api}/quiz`)
+
+    const response = await res.json()
+
+    return {
+      props: {
+        quizes: response,
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        quizes: null,
+      },
+    }
+  }
+}
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -4,35 +4,85 @@ import styled from 'styled-components'
 import Layout from '../components/Layout'
 import CardQuiz from '../components/CardQuiz'
 import { ButtonStyled } from '../components/Button/styled'
-import api from '../services/api'
-import { FiEdit, FiTrash } from 'react-icons/fi'
 import { UTILS } from '../constants/utils'
 import ReactLoading from 'react-loading'
 import theme from '../styles/theme'
+import Link from 'next/link'
 
 const HomeContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
+  div.main {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
 
-  @media (max-width: 480px) {
-    justify-content: center;
+    @media (max-width: 480px) {
+      justify-content: center;
+    }
+
+    div.loading {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      justify-self: center;
+    }
   }
 
-  div.loading {
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    justify-self: center;
+  div {
+    h1 {
+      margin-bottom: 60px;
+      font-size: 24px;
+    }
   }
 `
 
-const ButtonNewQuizContainer = styled.div`
-  padding: 40px 0;
+const ApresentationContainer = styled.div`
+  height: 100vh;
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: column;
+  justify-content: center;
+
+  > div {
+    width: 700px;
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 45px;
+      font-weight: 600;
+      margin-bottom: 30px;
+    }
+
+    span {
+      color: #94a2b3;
+    }
+  }
+
+  div {
+    margin-top: 35px;
+
+    a.get-start-link {
+      background-color: ${theme.colors.secondary};
+      padding: 15px 35px;
+      border-radius: 20px;
+      text-decoration: none;
+      color: ${theme.colors.contrastText};
+      margin-right: 20px;
+      font-weight: bold;
+    }
+
+    a:nth-child(2) {
+      text-decoration: none;
+      color: #299ef3;
+      margin-left: 20px;
+      font-weight: bold;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 `
 
 interface IPropsHome {
@@ -46,66 +96,61 @@ const HomePage: React.FC<IPropsHome> = ({ quizes }) => {
     return router.push(`quiz/?id=${id}`)
   }
 
-  const toggleMenuItems = [
-    {
-      icon: <FiTrash size={20} />,
-      text: 'Deletar',
-      onclick: async (id: string) => {
-        try {
-          await api.delete(`quiz/${id}`)
-
-          router.push(`/`)
-        } catch (error) {
-          alert(JSON.stringify({ err: error.message }))
-        }
-      },
-    },
-    {
-      icon: <FiEdit size={20} />,
-      text: 'Editar',
-      onclick: async (id: string) => {
-        try {
-          router.push(`/update/${id}`)
-        } catch (error) {
-          alert(JSON.stringify({ err: error.message }))
-        }
-      },
-    },
-  ]
-
   return (
-    <Layout>
-      <ButtonNewQuizContainer>
-        <ButtonStyled onClick={() => router.push('/create-quiz')} width="160px">
-          Novo quiz
-        </ButtonStyled>
-      </ButtonNewQuizContainer>
-      <HomeContainer>
-        {quizes ? (
-          quizes.map(item => (
-            <CardQuiz
-              header={item.title}
-              itemId={item.id}
-              toggleItems={toggleMenuItems}
-              background={item.img_bg_url}
-              width="350px"
-            >
-              <p>{item.title}</p>
-              <span>{item.description}</span>
-
-              <ButtonStyled onClick={() => handleSubmit(item.id)} type="button">
-                Responda o quiz
-              </ButtonStyled>
-            </CardQuiz>
-          ))
-        ) : (
-          <div className="loading">
-            <ReactLoading
-              color={theme.colors.secondary}
-              type="spinningBubbles"
-            />
+    <Layout home={true}>
+      <ApresentationContainer>
+        <div>
+          <div>
+            <h1>Building exactly the eCommerce website you want.</h1>
+            <span>
+              WooCommerce is a customizable, open-source eCommerce platform
+              built on WordPress. Get started quickly and make your way.
+            </span>
           </div>
-        )}
+          <div>
+            <Link href="#home">
+              <a className="get-start-link">Começar</a>
+            </Link>
+            ou
+            <Link href="/home">
+              <a>Crie um novo quiz</a>
+            </Link>
+          </div>
+        </div>
+      </ApresentationContainer>
+      <HomeContainer id="home">
+        <div>
+          <h1>Quizes mais recentes</h1>
+        </div>
+        <div className="main">
+          {quizes ? (
+            quizes.map(item => (
+              <CardQuiz
+                header={item.title}
+                itemId={item.id}
+                background={item.img_bg_url}
+                width="350px"
+              >
+                <p>{item.title}</p>
+                <span>{item.description}</span>
+
+                <ButtonStyled
+                  onClick={() => handleSubmit(item.id)}
+                  type="button"
+                >
+                  Responda o quiz
+                </ButtonStyled>
+              </CardQuiz>
+            ))
+          ) : (
+            <div className="loading">
+              <ReactLoading
+                color={theme.colors.secondary}
+                type="spinningBubbles"
+              />
+            </div>
+          )}
+        </div>
       </HomeContainer>
     </Layout>
   )
