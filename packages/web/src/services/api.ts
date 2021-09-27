2c8eb15821e4a56ff63b53f277bcb4c8227fc4c0
Wellington Santos <weltossousa@gmail.com>
2021-09-27 00:04:32 -0300

    adding interceptors to get token from server side or client side and pass in the headers

--- packages/web/src/services/api.ts
+++ packages/web/src/services/api.ts
@@ -1,8 +1,23 @@
 import axios from 'axios'
 import { UTILS } from '../constants/utils'
+import { parseCookies } from 'nookies'
 
-const api = axios.create({
-  baseURL: UTILS.api,
-})
+export const getApiClient = (ctx?: any) => {
+  const { ['quiz-auth.token']: token } = parseCookies(ctx)
 
-export default api
+  const api = axios.create({
+    baseURL: UTILS.api,
+  })
+
+  api.interceptors.request.use(config => {
+    console.log(config)
+
+    return config
+  })
+
+  if (token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return api
+}
