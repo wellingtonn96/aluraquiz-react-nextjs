7d5c5b9cfacf43cbf3cd32a2fd913cb612cfdf63
Wellington Santos <weltossousa@gmail.com>
2021-10-04 00:49:00 -0300

    remove github corner

--- packages/web/src/components/Layout/index.tsx
+++ packages/web/src/components/Layout/index.tsx
@@ -1,59 +1,14 @@
 import React from 'react'
-import { UTILS } from '../../constants/utils'
-import GithubCorner from 'react-github-corner'
-import { Background, HeaderContainer } from './style'
-import Link from 'next/link'
-import { useAuth } from '../../hooks/Auth'
+import { Background } from './style'
+import Header from './Header'
 
-interface IPropsLayout {
+const Layout: React.FC<{
   background?: string
-  cornerColor?: string
   padding?: boolean
-  home?: boolean
-}
-
-const Layout: React.FC<IPropsLayout> = ({
-  children,
-  background,
-  cornerColor,
-  padding,
-  home,
-}) => {
-  const { user } = useAuth()
+}> = ({ children, background, padding }) => {
   return (
     <Background background={background} padding={padding}>
-      <HeaderContainer>
-        <ul>
-          {!user && (
-            <>
-              {!home ? (
-                <li>
-                  <Link href="/">
-                    <a>Home</a>
-                  </Link>
-                </li>
-              ) : (
-                <>
-                  <li>
-                    <Link href="/sign-up">
-                      <a>Sign Up</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/sign-in">
-                      <a>Sign In</a>
-                    </Link>
-                  </li>
-                </>
-              )}
-            </>
-          )}
-        </ul>
-      </HeaderContainer>
-      <GithubCorner
-        bannerColor={cornerColor ? cornerColor : UTILS.theme.colors.mainBg}
-        href={UTILS.github}
-      />
+      <Header />
       <div>{children}</div>
     </Background>
   )
